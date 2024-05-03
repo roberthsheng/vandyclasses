76527a2714ec6841a8a7b991f8e1f664c3d82e7c
roberthsheng <roberthsheng@gmail.com>
2024-05-02 21:16:14 -0400

    v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/roberthsheng/vandyclasses
+
+go 1.22.2
+
+require github.com/go-redis/redis v6.15.9+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+    "fmt"
+    "html/template"
+    "net/http"
+    "strings"
+
+    "github.com/go-redis/redis"
+)
+
+var redisClient *redis.Client
+
+func main() {
+    redisClient = redis.NewClient(&redis.Options{
+        Addr: "redis-16488.c284.us-east1-2.gce.redns.redis-cloud.com:16488",
+		// password goes here
+    })
+
+    // test redis connection
+    _, err := redisClient.Ping().Result()
+    if err != nil {
+        panic(err)
+    }
+
+    http.HandleFunc("/", searchHandler)
+    fmt.Println("Server is running on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        // render the search form
+        tmpl := template.Must(template.ParseFiles("search.html"))
+        tmpl.Execute(w, nil)
+    } else if r.Method == "POST" {
+        // get search query from form
+        query := r.FormValue("query")
+
+		// search and render
+        matches := searchRedis(query)
+        tmpl := template.Must(template.ParseFiles("results.html"))
+        tmpl.Execute(w, matches)
+    }
+}
+
+func searchRedis(query string) []map[string]string {
+    var matches []map[string]string
+
+    keys, err := redisClient.Keys("*").Result()
+    if err != nil {
+        return matches
+    }
+
+	// pipeline to get all keys and vals
+    pipe := redisClient.Pipeline()
+    for _, key := range keys {
+        pipe.Get(key)
+    }
+    values, err := pipe.Exec()
+    if err != nil {
+        return matches
+    }
+
+    for i, key := range keys {
+        value, ok := values[i].(*redis.StringCmd).Val(), true
+        if !ok {
+            continue
+        }
+
+        // check if key or value contains the query
+        if strings.Contains(strings.ToLower(key), strings.ToLower(query)) ||
+            strings.Contains(strings.ToLower(value), strings.ToLower(query)) {
+            match := map[string]string{
+                "key":   key,
+                "value": value,
+            }
+            matches = append(matches, match)
+        }
+    }
+
+    return matches
+}
